25ab4113f8943b9cf88a700f3ee98e8d77cd8648
Joseph Panetta <jlpanetta1681@icloud.com>
2021-04-07 11:35:22 -0400

    Workin on adding the necessary components

--- src/index.js
+++ src/index.js
@@ -2,9 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class Grid extends React.Component {
+  render() {
+    return (
+      <div>
+        Grid
+      </div>
+    );
+  }
+}
 
+class Main extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      generation: 0,
+    }
+  }
 
-ReactDOM.render(<p>Hello World</p>,
+  render() {
+    return (
+      <div>
+        <h1>The Game Of Life</h1>
+        <Grid
+        />
+        <h2>Generations: {this.state.generation}</h2>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <main />,
   document.getElementById('root')
 );
 
