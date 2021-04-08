3acef16b92b8770e5f1f3321f026d67ec12dbe10
Joseph Panetta <jlpanetta1681@icloud.com>
2021-04-08 09:29:24 -0400

    game is functioning as expected, working on some visual styling now

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { ButtonToolbar, MenuItem, DropdownButton } from 'react-bootstrap'
-import { Dropdown } from "react-bootstrap";
+import { ButtonToolbar, Dropdown, DropdownButton } from 'react-bootstrap';
 
 class Box extends React.Component {
   selectBox = () => {
@@ -22,7 +21,7 @@ class Box extends React.Component {
 
 class Grid extends React.Component {
   render() {
-    const width = (this.props.cols * 14) + 1;
+    const width = (this.props.cols * 14);
     let rowsArr = [];
 
     let boxClass = "";
@@ -82,9 +81,9 @@ class Buttons extends React.Component {
             id="size-menu"
             onSelect={this.handleSelect}
           >
-            <menuItem eventKey="1">20x10</menuItem>
-            <menuItem eventKey="1">50x30</menuItem>
-            <menuItem eventKey="1">70x50</menuItem>
+            <Dropdown.Item eventKey="1">20x10</Dropdown.Item>
+            <Dropdown.Item eventKey="2">50x30</Dropdown.Item>
+            <Dropdown.Item eventKey="3">70x50</Dropdown.Item>
           </DropdownButton>
         </ButtonToolbar>
       </div>
@@ -135,6 +134,42 @@ class Main extends React.Component {
   pauseButton = () => {
     clearInterval(this.intervalId);
   }
+
+
+  slow = () => {
+    this.speed = 1000;
+    this.playButton();
+  }
+
+  fast = () => {
+    this.speed = 100;
+    this.playButton();
+  }
+  clear = () => {
+    var grid = Array(this.rows).fill().map(() => Array(this.cols).fill(false));
+    this.setState({
+      gridFull: grid,
+      generation: 0
+    });
+  }
+  gridSize = (size) => {
+    switch (size) {
+      case "1":
+        this.cols = 20;
+        this.rows = 10;
+        break;
+      case "2":
+        this.cols = 50;
+        this.rows = 30;
+        break;
+      default:
+        this.cols = 70;
+        this.rows = 50;
+    }
+    this.clear();
+
+  }
+
 
   play = () => {
     let g = this.state.gridFull;
